17dae01194304cec9914e7342af3999a4f630182
onifade <onifadejoshua15@gmial.com>
2022-05-18 13:11:47 +0100

    first commit

---
+++ script_folder/history.js
@@ -0,0 +1,32 @@
+"use strict";
+const history = [{
+    accountName: "Thompson Bill",
+    accountNumber: 2254854309,
+    bankName: "Zenith Bank",
+    amount: 90000,
+    paymentStatus: "Sent"
+}, {
+    accountName: "Owolabi Daniel",
+    accountNumber: 7454854309,
+    bankName: "Keystone Bank",
+    amount: 87000,
+    paymentStatus: "Pending"
+}, {
+    accountName: "Obayan Joshua",
+    accountNumber: 6054854105,
+    bankName: "Uba Bank",
+    amount: 25000,
+    paymentStatus: "Declined"
+}, {
+    accountName: "Oyedepo David",
+    accountNumber: 2254853305,
+    bankName: "Gt Bank",
+    amount: 38000,
+    paymentStatus: "Pending"
+}, {
+    accountName: "Adekoya Abayomi",
+    accountNumber: 2254814709,
+    bankName: "First Bank",
+    amount: 60000,
+    paymentStatus: "Sent"
+}];
---
+++ script_folder/script.js
@@ -0,0 +1,24 @@
+"use strict";
+const sideMenu = document.querySelector("aside");
+const menuBtn = document.querySelector("#menu-btn");
+const closeBtn = document.querySelector("#close-btn");
+const themeToggler = document.querySelector(".theme-toggler");
+//Show sidebar
+menuBtn.addEventListener("click", () => {
+        sideMenu.style.display = "block";
+    })
+    //Close Sidebar
+closeBtn.addEventListener("click", () => {
+        sideMenu.style.display = "none";
+    })
+    //Change theme
+themeToggler.addEventListener("click", () => {
+    document.body.classList.toggle("dark-theme-variables");
+});
+themeToggler.addEventListener("click", () => {
+    document.body.classList.toggle(".dark-theme-variables");
+
+    themeToggler.querySelector("i:nth-child(1)").classList.toggle("active");
+
+    themeToggler.querySelector("i:nth-child(2)").classList.toggle("active");
+})
